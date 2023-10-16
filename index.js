df34a518447a6f3a9c1eea19e1b5b202232a690d
Talha Husnain <110324840+Malik-444@users.noreply.github.com>
2023-10-16 12:42:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// console.log("this is my first nodeSJ app")
+// console.log("this is second ")
+// console.log("Third ")
+
+const { data } = require("./car");
+// console.log(data);
+console.log(JSON.stringify(data, null, 2));
